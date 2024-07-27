255ddd50f953890cbe8aca38516bfad8f1639e29
Karim Toulba <karim@wadipurple.com>
2024-07-27 12:10:43 +0300

    Update play1.js

--- play1.js
+++ play1.js
@@ -4,8 +4,12 @@ Author: Karim Toulba
 Date: 27/07/2024
 */
 
-// PRINT
+// Print
 console.log(); // Print command
 console.log(99); // Print 99
 console.log('Hello World!'); // Print Hello World!
+
+// Arithmetic Operators
+console.log(3.5 + 34); // plus / minus / multiply / divide
+console.log(11 % 3); // modulus remaining
 
