72ac0ea91f39fb2cd500c9b0baa0bf25c3dda98a
Karim Toulba <karim@wadipurple.com>
2024-07-27 16:57:29 +0300

    Update play2.js

--- play2.js
+++ play2.js
@@ -57,3 +57,16 @@ if (stopLight === 'red') {
   console.log('Caution, unknown!');
 }
 
+// The Switch Keyword
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place': console.log('You get the gold medal!');
+  break;
+  case 'second place': console.log('You get the silver medal!');
+  break;
+  case 'third place': console.log('You get the bronze medal!');
+  break;
+  default: console.log('No medal awarded.');
+  break;
+}
+
