5b68b76423fedaa90ceb266ff05f3a2e6606fd82
Karim Toulba <karim@wadipurple.com>
2024-07-27 15:28:35 +0300

    Update play2.js

--- play2.js
+++ play2.js
@@ -4,3 +4,11 @@ Author: Karim Toulba
 Date: 27/07/2024
 */
 
+// If Else Statement - True or False Variable
+let sale = true;
+sale = false;
+if (sale) {
+  console.log('Time to buy');
+} else {
+  console.log('Time to wait for a sale');
+}
