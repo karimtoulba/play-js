7e20311ca312db943adeb695c2c89bc001e80da1
Karim Toulba <karim@wadipurple.com>
2024-07-28 22:09:50 +0300

    Update play4.js

--- play4.js
+++ play4.js
@@ -50,11 +50,13 @@ do {
 } while (counter < 5);
 
 // Break
-for (let i = 0; i <5; i++) {
-  if (i > 2) {
+for (let i = 0; i < rapperArray.length; i++) {
+  if (rapperArray[i] === 'Notorious B.I.G.') {
+    console.log(rapperArray[i]);
     break;
   }
-  console.log(i);
+  console.log(rapperArray[i]);
 }
+console.log('And if you don\'t know, now you know.');
 
 
