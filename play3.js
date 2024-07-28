bf49728f2036b9a91cec1936753aadee1bb09487
Karim Toulba <karim@wadipurple.com>
2024-07-28 15:05:03 +0300

    Update play3.js

--- play3.js
+++ play3.js
@@ -1,1 +1,17 @@
+/* 
+File: play3.js
+Author: Karim Toulba
+Date: 28/07/2024
+*/
+
+// Global Scope Variables - can be used everywhere
+let satellite = 'The Moon'; // Global Scope var
+let galaxy = 'The Milky Way'; // Global Scope var
+let stars = 'North Star'; // Global Scope var
+
+function callMyNightSky () {
+  return `Night Sky: ${satellite}, ${galaxy}, and ${stars}.`;
+}
+
+console.log(callMyNightSky());
 
