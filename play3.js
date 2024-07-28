30aa8f6937ac1d3385975cec0913aa5ee618141d
Karim Toulba <karim@wadipurple.com>
2024-07-28 15:32:42 +0300

    Update play3.js

--- play3.js
+++ play3.js
@@ -8,10 +8,20 @@ Date: 28/07/2024
 let satellite = 'The Moon'; // Global Scope var
 let galaxy = 'The Milky Way'; // Global Scope var
 let stars = 'North Star'; // Global Scope var
-
 function callMyNightSky () {
   return `Night Sky: ${satellite}, ${galaxy}, and ${stars}.`;
 }
-
 console.log(callMyNightSky());
 
+// Block Scope Variables - cannot be used everywhere, only in the function determined
+function logVisibleLightWaves () {
+  const lightWaves = 'Moonlight';
+  console.log(lightWaves);
+}
+logVisibleLightWaves(); // prints Moonlight.
+console.log(lightWaves); // Error as the variable is block scope.
+
+// Array
+let shoppingList = ['Wasser', 'Brot', 'Milch', 'Kase'];
+console.log(shoppingList[0]); // prints Wasser
+console.log(shoppingList[2]); // prints Milch
