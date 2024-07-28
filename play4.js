4a91b6f41992b4e4a9ed9a063fdd17c3f36356e6
Karim Toulba <karim@wadipurple.com>
2024-07-28 19:41:22 +0300

    Update play4.js

--- play4.js
+++ play4.js
@@ -4,7 +4,12 @@ Author: Karim Toulba
 Date: 28/07/2024
 */
 
-// For Loops
+// For Loop - FORWARD
 for (let counter = 5; counter <= 10; counter++) {
+  console.log(counter);
+}
+
+// For Loop - BACKGROUND
+for (let counter = 5; counter >= 0; counter--) {
   console.log(counter);
 }
