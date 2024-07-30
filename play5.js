8877ac45503e96776665315172132298bd66f41d
Karim Toulba <karim@wadipurple.com>
2024-07-30 23:39:45 +0300

    Update play5.js

--- play5.js
+++ play5.js
@@ -38,3 +38,16 @@ const foundIndex = myCities.findIndex(
   }
 );
 
+// reduce => method returns a single value after iterating through the elements of an array.
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce(
+  function (accumulator, currentValue) {
+    console.log('The value of accumulator: ', accumulator);
+    console.log('The value of currentValue: ', currentValue);
+    return accumulator + currentValue;
+  }, 10
+);
+
+console.log(newSum);
+
