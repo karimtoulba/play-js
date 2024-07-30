d52579c7381021d2538d22ddb65d689b590c1b82
Karim Toulba <karim@wadipurple.com>
2024-07-30 22:56:20 +0300

    Update play5.js

--- play5.js
+++ play5.js
@@ -1,1 +1,13 @@
+/* 
+File name: play5.js
+Author: Karim Toulba
+Date: 30/07/2024
+*/
 
+// forEach
+let myCities = ['Cairo', 'Alexandria', 'Aswan'];
+myCities.forEach(
+  function (item) {
+    console.log(`I want to go to ${item}`);
+  }
+);
