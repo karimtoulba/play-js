1de2f85a19622f22937851e64134200672eb067a
Karim Toulba <karim@wadipurple.com>
2024-08-09 18:22:02 +0300

    Create 05-Others.js

---
+++ JS/05-Others.js
@@ -0,0 +1,7 @@
+// Priting the current date in year, month, day format
+const currentDate = new Date();
+console.log(currentDate);
+
+// Printing specific year
+const currentYear = date.getFullYear();
+console.log(`Year: ${currentYear}`);
