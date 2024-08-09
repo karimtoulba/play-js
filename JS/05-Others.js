3bb5324676ccab7fe5c3c4a6a4a46bcac05e2bcf
Karim Toulba <karim@wadipurple.com>
2024-08-09 18:54:51 +0300

    Update 05-Others.js

--- JS/05-Others.js
+++ JS/05-Others.js
@@ -10,3 +10,10 @@ console.log(`${day} - ${month} - ${year}`);
 // Change & Increment Date
 date.setDate(date.getDate() + 1);
 console.log(date); // prints current date +1 incremented.
+
+// setTimeOut and setInterval
+function every() { console.log('Executed every 3 seconds); }
+function after() { console.log('Executed after 3 seconds); }
+
+setTimeOut(after, 3000); // prints the message after 3 seconds.
+setInterval(every, 3000); // prints the message every 3 seconds.
