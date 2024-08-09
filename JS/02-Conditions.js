74b67237cbe2592a1b482226f93cba0d4a6cb23c
Karim Toulba <karim@wadipurple.com>
2024-08-09 18:15:04 +0300

    Update 02-Conditions.js

--- JS/02-Conditions.js
+++ JS/02-Conditions.js
@@ -95,3 +95,64 @@ const var = (rows, columns) => {} // function with two parameters
 const var = rows => {} // function with one parameter
 var(rows); // calling the function
 const var = height => height*height; // instead of return for single line
+
+// For Loop - FORWARD
+for (let counter = 5; counter <= 10; counter++) {
+  console.log(counter);
+}
+
+// For Loop - BACKGROUND
+for (let counter = 5; counter >= 0; counter--) {
+  console.log(counter);
+}
+
+// For Loop - ARRAY
+let myCities = ['Amsterdam', 'Berlin', 'Oslo'];
+for (let i = 0; i < myCities.length; i++) {
+  console.log(myCities[i]);
+}
+
+// Nested For Loop
+let oneNames = ['Ahmed', 'Karim', 'Hussein', 'Tarek'];
+let twoNames = ['Ahmed', 'Ali', 'Ismail', 'Karim'];
+let hiNames = [];
+
+for (let i = 0; i < oneNames.length; i++) {
+  for (let j= 0; j < twoNames.length; j++) {
+    if (oneNames[i] === twoNames[j]) {
+      hiNames.push(oneNames[i]);
+    }
+  }
+}
+
+console.log(hiNames);
+
+// While Loop
+let counter = 0;
+while (counter < 5) {
+  console.log('');
+  counter++;
+}
+
+// Do While 
+let counter = 0;
+do {
+  console.log(''); 
+  counter++;
+} while (counter < 5);
+
+// Break
+for (let i = 0; i < rapperArray.length; i++) {
+  if (rapperArray[i] === 'Notorious B.I.G.') {
+    console.log(rapperArray[i]);
+    break;
+  }
+  console.log(rapperArray[i]);
+}
+console.log('And if you don\'t know, now you know.');
+
+// Functions as Data
+const newFunction = oldFunction; // rename the function
+newFunction(); // call the old function with the new name
+console.log(newFunction.name); // retreive the name of the original old function
+
